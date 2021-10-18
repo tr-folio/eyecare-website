5304aea35c331997cc1c181d3f6aa05b2b66b713
tr-folio <tahrimurmoon@gmail.com>
2021-10-18 22:55:56 +0600

    header added with navbar

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
 import { 
   BrowserRouter as Router,
   Switch,
-  Route,
-  Link
+  Route
 } from 'react-router-dom';
 import './App.css';
+import Header from './Components/Header/Header';
 import Home from './Components/Home/Home';
 
 function App() {
   return (
     <>
       <Router>
+        <Header></Header>
         <Switch>
           <Route path="/home">
             <Home></Home>
---
+++ src/Components/Header/Header.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { Container, Nav, Navbar } from "react-bootstrap";
+import { HashLink } from "react-router-hash-link";
+
+const Header = () => {
+    return (
+        <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+            <Container>
+                <Navbar.Brand href="#home">EyeCare-Website</Navbar.Brand>
+                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                <Navbar.Collapse id="responsive-navbar-nav" className="d-flex-lg justify-content-end">
+                    <Nav>
+                        <Nav.Link as={HashLink} to="/home">Home</Nav.Link>
+                        <Nav.Link as={HashLink} to="#memes">
+                            Dank memes
+                        </Nav.Link>
+                    </Nav>
+                </Navbar.Collapse>
+            </Container>
+        </Navbar>
+    );
+}
+
+export default Header;
