ff62c819a49ba6ff74a77e51b1a10a347dd9ab3e
tr-folio <tahrimurmoon@gmail.com>
2021-10-18 21:57:14 +0600

    react router added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
+import { 
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from 'react-router-dom';
 import './App.css';
 import Home from './Components/Home/Home';
 
 function App() {
   return (
     <>
-      <Home></Home>
+      <Router>
+        <Switch>
+          <Route path="/home">
+            <Home></Home>
+          </Route>
+          <Route exact path="/">
+            <Home></Home>
+          </Route>
+        </Switch>
+      </Router>
     </>
   );
 }
